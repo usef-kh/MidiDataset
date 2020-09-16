4a2583d0205c5d74b9868d87ac39cac48fb73873
usef-kh <yousif.khaireddin@gmail.com>
2020-09-16 14:13:34 -0400

    changed batch size'

--- batches/generate_song_batches.py
+++ batches/generate_song_batches.py
@@ -8,7 +8,8 @@ songs = []
 for index, row in database.iterrows():
     songs += [row['Song Name'] + ' - ' + row['Artist']]
 
-n_batches = (len(database) - 1) // 100 + 1
+batch_size = 70
+n_batches = ((len(database) - 1) // batch_size) + 1
 batches = np.array_split(np.array(songs), n_batches)
 # print(len(batches))
 for i, batch in enumerate(batches):
